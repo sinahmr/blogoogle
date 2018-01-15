bacb60d383985b790ef1634e1bc8eeb0a259d5a5
Sina Hajimiri <sina.hajimiri@gmail.com>
2018-01-15 18:55:38 +0330

    Ignore posts which their blog is ignored

--- crawler/scraper/pipelines.py
+++ crawler/scraper/pipelines.py
@@ -7,21 +7,27 @@ from scrapy.exceptions import DropItem
 
 class CheckerPipeline(object):
     def __init__(self):
-        self.blogs_seen = set()
+        self.seen_blogs = set()
+        self.ignored_blogs = set()
 
     def process_item(self, item, spider):
+        blog_name = item['blog_url'].split('.blog.ir')[0].split('/')[-1]
         if item['type'] == 'blog':
-            if len(self.blogs_seen) == spider.n:
+            if len(self.seen_blogs) == spider.n:
                 # posts remaining in the queue are not processed by using the line below, so this isn't good
                 # spider.crawler.engine.close_spider(spider)
                 spider.continue_crawling_blogs = False
+                self.ignored_blogs.add(blog_name)
                 raise DropItem(
                     '(%s) It is enough for blogs themselves, now we only continue creating queued posts and then exit' %
                     item['blog_url'])
-            elif item['blog_url'] in self.blogs_seen:
+            elif blog_name in self.seen_blogs:
                 raise DropItem('Duplicate url found: %s' % item['blog_url'])
             else:
-                self.blogs_seen.add(item['blog_url'])
+                self.seen_blogs.add(blog_name)
+        else:  # item['type'] == 'post'
+            if blog_name in self.ignored_blogs:
+                raise DropItem("This post's blog was ignored, so it will be ignored. Blog: %s" % blog_name)
         return item
 
 
